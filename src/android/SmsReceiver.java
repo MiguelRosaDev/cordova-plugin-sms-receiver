26489588a283048c7ef0c164e6eed32b0315ab0c
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2024-12-13 09:31:00 +0000

    Update SmsReceiver.java

--- src/android/SmsReceiver.java
+++ src/android/SmsReceiver.java
@@ -21,34 +21,67 @@ public class SmsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context ctx, Intent intent) {
+        //Android 15
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            if (SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.getAction())) {
+            Bundle extras = intent.getExtras();
+            Status status = (Status) extras.get(SmsRetriever.EXTRA_STATUS);
+            if (status.getStatusCode() == CommonStatusCodes.SUCCESS) {
+                String message = extras.getString(SmsRetriever.EXTRA_SMS_MESSAGE);
+                String otpCode = extractOtpFromMessage(message);
 
-        // Get the SMS map from Intent
-        Bundle extras = intent.getExtras();
-        if (extras != null) {
-            // Get received SMS Array
-            Object[] smsExtra = (Object[]) extras.get(SMS_EXTRA_NAME);
-
-            for (int i = 0; i < smsExtra.length; i++) {
-                SmsMessage sms = SmsMessage.createFromPdu((byte[]) smsExtra[i]);
-                if (this.isReceiving && this.callbackReceive != null) {
-                    JSONObject jsonObj = new JSONObject();
-                    try {
-                        jsonObj.put("messageBody", sms.getMessageBody());
-                        jsonObj.put("originatingAddress", sms.getOriginatingAddress());
-                    } catch (Exception e) {
-                        System.out.println("Error: " + e);
-                    }
-                    PluginResult result = new PluginResult(PluginResult.Status.OK, jsonObj);
-                    result.setKeepCallback(true);
-                    callbackReceive.sendPluginResult(result);
+                JSONObject jsonObj = new JSONObject();
+                try {
+                    jsonObj.put("otpCode", otpCode);
+                    jsonObj.put("messageBody", message);
+                } catch (Exception e) {
+                    System.out.println("Error: " + e);
                 }
-            }
-
-            // If the plugin is active and we don't want to broadcast to other receivers
-            if (this.isReceiving && !broadcast) {
-                this.abortBroadcast();
+                PluginResult result = new PluginResult(PluginResult.Status.OK, jsonObj);
+                result.setKeepCallback(true);
+                callbackReceive.sendPluginResult(result);
             }
         }
+        } else {
+            //Android 14 and bellow
+            // Get the SMS map from Intent
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                // Get received SMS Array
+                Object[] smsExtra = (Object[]) extras.get(SMS_EXTRA_NAME);
+    
+                for (int i = 0; i < smsExtra.length; i++) {
+                    SmsMessage sms = SmsMessage.createFromPdu((byte[]) smsExtra[i]);
+                    if (this.isReceiving && this.callbackReceive != null) {
+                        JSONObject jsonObj = new JSONObject();
+                        try {
+                            jsonObj.put("messageBody", sms.getMessageBody());
+                            jsonObj.put("originatingAddress", sms.getOriginatingAddress());
+                        } catch (Exception e) {
+                            System.out.println("Error: " + e);
+                        }
+                        PluginResult result = new PluginResult(PluginResult.Status.OK, jsonObj);
+                        result.setKeepCallback(true);
+                        callbackReceive.sendPluginResult(result);
+                    }
+                }
+    
+                // If the plugin is active and we don't want to broadcast to other receivers
+                if (this.isReceiving && !broadcast) {
+                    this.abortBroadcast();
+                }
+            }
+        }
+    }
+
+    private String extractOtpFromMessage(String message) {
+        // Extraia o código OTP usando regex (personalize conforme necessário)
+        Pattern otpPattern = Pattern.compile("\\b\\d{6}\\b"); // Captura 6 dígitos
+        Matcher matcher = otpPattern.matcher(message);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return null;
     }
 
     public void broadcast(boolean v) {
